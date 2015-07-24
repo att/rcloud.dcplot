c7399a9959694d9042db9aa727dd72266a81b5c5
Gordon Woodhull <gordon@woodhull.com>
2015-07-24 15:59:13 -0400

    refactor wdcplot.substitute

--- R/wdcplot.R
+++ R/wdcplot.R
@@ -1,8 +1,11 @@
 wdcplot.special.variable <- function(name) structure(name, wdcplot.placeholder = "special")
 wdcplot.column <- function(name) structure(name, wdcplot.placeholder = "column")
 
-wdcplot <- function(data, dims=NULL, groups=NULL, charts=NULL)
-{
+wdcplot.default.context <- NULL
+
+wdcplot.substitute <- function(context, expr) {
+  data <- context$data
+
   # make a pseudo-environment which maps columns and special variables to placeholders
   specials <- list(..index.. = wdcplot.special.variable('index'),
                    ..value.. = wdcplot.special.variable('value'),
@@ -12,7 +15,7 @@ wdcplot <- function(data, dims=NULL, groups=NULL, charts=NULL)
   looksee <- list2env(c(cols2placeholders, specials))
 
   # this feint comes from pryr: make substitute work in the global environment
-  parent.env <- parent.frame();
+  parent.env <- parent.frame(2);
   if (identical(parent.env, globalenv())) {
     parent.env <- as.list(parent.env)
   }
@@ -21,17 +24,20 @@ wdcplot <- function(data, dims=NULL, groups=NULL, charts=NULL)
   # - first evaluate anything bquoted with .(expr)
   # - then substitute in the dataframe pseudo-environment
   # - then substitute in the parent environment
-  bfp <- function(sexpr, penv)
-    do.call(substitute,
-            list(do.call(substitute,
-                         list(do.call(bquote, list(sexpr, where = parent.frame(2))),
-                              looksee)),
-                 parent.env))
+  do.call(substitute,
+          list(do.call(substitute,
+                       list(do.call(bquote, list(expr, where = parent.env)),
+                            looksee)),
+               parent.env))
 
-  # Enable use of R variables as parameters in definitions, i.e. width = mywidth
-  dims2 <- bfp(substitute(dims))
-  groups2 <- bfp(substitute(groups))
-  charts2 <- bfp(substitute(charts))
+}
+
+wdcplot <- function(data, dims=NULL, groups=NULL, charts=NULL) {
+  context <- list(data=data)
+
+  dims2 <- wdcplot.substitute(context, substitute(dims))
+  groups2 <- wdcplot.substitute(context, substitute(groups))
+  charts2 <- wdcplot.substitute(context, substitute(charts))
 
   deferred.rcloud.result(function() dcplot.caps$handle_dcplot(list("dcplot", data, dims2, groups2, charts2)))
 }
